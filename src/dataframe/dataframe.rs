e176662127ce1d3c5c18061f10ad5f2c627a7afc
Jamie Blondin <jblondin@gmail.com>
2017-05-09 10:46:56 -0400

    Added basic test (with random fake data)

--- src/dataframe/dataframe.rs
+++ src/dataframe/dataframe.rs
@@ -31,7 +31,11 @@ impl DataFrame {
         let untransformed_data = try!(extract_data(&mut reader, &used_fields));
         let transformed_data = try!(transform_data(&untransformed_data, &config));
 
-        return Ok((config, DataFrame { data: transformed_data } as DataFrame))
+        return Ok((config, DataFrame { data: transformed_data }))
+    }
+
+    pub fn fieldnames(&self) -> Vec<String> {
+        self.data.fields.iter().map(|fi| fi.name.clone()).collect()
     }
 
     pub fn as_matrix(&self) -> Result<(Vec<String>, Matrix), DataFrameError> {
@@ -341,4 +345,53 @@ fn transform_data(untransformed_data: &DataStore, config: &Config)
         }
     }
     Ok(tf_data)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    macro_rules! test_data_path {
+        () => {{
+            PathBuf::from(file!()) // current file
+                .parent().unwrap() // "dataframe" directory
+                .parent().unwrap() // "src" directory
+                .parent().unwrap() // etl crate root directory;
+                .join("test_data")
+        }}
+    }
+
+    #[test]
+    fn basic_test() {
+        let data_dir_pathbuf = test_data_path!();
+        let data_file_path = data_dir_pathbuf.join("people.csv");
+        let config_file_path = data_dir_pathbuf.join("people.yaml");
+
+        let (config, df) = DataFrame::load(&config_file_path, &data_file_path).unwrap();
+        println!("{:#?}", config);
+        println!("{:#?}", df.data.fields);
+        assert_eq!(df.nrows(), 99);
+        let fns = df.fieldnames();
+        assert_eq!(fns.len(), 10);
+        println!("{:#?}", fns);
+    }
+
+    #[test]
+    fn matrix_test() {
+        let data_dir_pathbuf = test_data_path!();
+        let data_file_path = data_dir_pathbuf.join("matrix_test.csv");
+        let config_file_path = data_dir_pathbuf.join("matrix_test.yaml");
+
+        let (config, df) = DataFrame::load(&config_file_path, &data_file_path).unwrap();
+        println!("{:#?}", config);
+        assert_eq!(df.nrows(), 100);
+
+        let (fieldnames, mat) = df.as_matrix().unwrap();
+        println!("{:#?}", fieldnames);
+        println!("{:#?}", mat);
+        assert_eq!(fieldnames.len(), 2);
+        assert_eq!(mat.nrows(), 100);
+        assert_eq!(mat.ncols(), 2);
+    }
 }
